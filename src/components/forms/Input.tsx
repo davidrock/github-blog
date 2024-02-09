459afda02a1fae88c966c4aae799f96c4b8879ce
David Martinelli <david.martinelli@farfetch.com>
2024-02-09 12:21:50 +0000

    create post preview for Blog homepage

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -7,7 +7,7 @@ export function Input(props: InputProps) {
     <div className="flex">
       <input
         {...props}
-        className="flex h-10 px-4 py-5 w-full rounded-md bg-base-input text-sm transition-colors placeholder:text-base-label focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring ring-blue disabled:cursor-not-allowed disabled:opacity-50"
+        className="flex h-10 px-4 py-5 w-full rounded-md bg-base-input text-xs transition-colors placeholder:text-base-label focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring ring-blue disabled:cursor-not-allowed disabled:opacity-50"
       />
     </div>
   );
--- src/pages/Blog/components/BlogHeader.tsx
+++ src/pages/Blog/components/BlogHeader.tsx
@@ -9,7 +9,7 @@ export function BlogHeader() {
         <EffectSquaresRight />
         <div className="absolute top-0 left-0 -mt-16 bg-[radial-gradient(ellipse_at_center,_#0078f0_0%,#071422_70%)] rounded-full opacity-5 h-[255px] w-[255px]"></div>
         <div className="absolute top-0 right-0 -mt-16 bg-[radial-gradient(ellipse_at_center,_#0078f0_0%,#071422_70%)] rounded-full opacity-5 h-[255px] w-[255px]"></div>
-        <div className="absolute backdrop-brightness-100 mx-auto -mb-32 bg-[radial-gradient(ellipse_at_center,_#455689_10%,#0B1B2B_60%)] opacity-15 h-40 w-screen"></div>
+        <div className="absolute backdrop-brightness-100 mx-auto -mb-32 bg-[radial-gradient(ellipse_at_center,_#455689_10%,#0B1B2B_60%)] opacity-15 h-40 w-full"></div>
       </div>
     </div>
   );
